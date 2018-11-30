85437244f89ebb0a51b44e1e5d2132a5b503c5d0
MaxEhs <ehringha@stud.fra-uas.de>
2018-11-30 21:52:34 +0100

    Parameter umbenannt - Max

--- GUI/src/Klima.java
+++ GUI/src/Klima.java
@@ -6,16 +6,16 @@ public class Klima {
 	private boolean heizungsstatus;
 	private boolean ACstatus;
 
-	public void set_heizungsstatus(boolean s1) {
-		heizungsstatus = s1;
+	public void set_heizungsstatus(boolean status) {
+		heizungsstatus = status;
 	}
 
 	public boolean get_heizungsstatus() {
 		return heizungsstatus;
 	}
 
-	public void set_ACstatus(boolean s2) {
-		ACstatus = s2;
+	public void set_ACstatus(boolean status) {
+		ACstatus = status;
 	}
 
 	public boolean get_ACstatus() {
