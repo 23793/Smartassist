9dd15033878c0b7593c6e0fc3e30600bb80e7cfa
MaxEhs <ehringha@stud.fra-uas.de>
2018-12-02 22:48:48 +0100

    updateRooms Methode

--- GUI/Main.java
+++ GUI/Main.java
@@ -4,6 +4,7 @@ import GUI.src.*;
 
 public class Main {
 	private static Schnittstelle s = new Schnittstelle();
+	private static Raum r1 = new Raum(), r2 = new Raum(), r3 = new Raum();
 
 	public static void main(String[] args) {
 		s.connect();
@@ -17,14 +18,28 @@ public class Main {
 	private void updateRooms() {
 		String roomString;
 		String roomAttributes[];
+		Raum t1, t2, t3;
 
 		s.connect();
 		roomString = s.receive(); // Gets the room data String
 		s.close();
 
 		roomAttributes = roomString.split(";");
-		
-		// Check if rooms exist
+
+		// "id1;temp1;light1;switch1;id2;temp2;light2;switch2;id3;temp3;light3;switch3;
+
+		// Creating temp rooms
+		t1 = new Raum(Integer.parseInt(roomAttributes[0]), Float.parseFloat(roomAttributes[1]),
+				Integer.parseInt(roomAttributes[2]), Integer.parseInt(roomAttributes[3]));
+		t2 = new Raum(Integer.parseInt(roomAttributes[4]), Float.parseFloat(roomAttributes[5]),
+				Integer.parseInt(roomAttributes[6]), Integer.parseInt(roomAttributes[7]));
+		t3 = new Raum(Integer.parseInt(roomAttributes[8]), Float.parseFloat(roomAttributes[9]),
+				Integer.parseInt(roomAttributes[10]), Integer.parseInt(roomAttributes[11]));
+
+		// Muss hier noch eine Logik hin?
+		r1 = t1;
+		r2 = t2;
+		r3 = t3;
 	}
 }
 
